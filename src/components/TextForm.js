b52a5c04341113d37de0f6d49d7b21556d560b1a
ManthanKumar1 <manthantanwar1@gmail.com>
2024-10-04 01:46:31 +0530

    text form

---
+++ src/components/TextForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import propTypes from 'prop-types'
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        let newText = text.toUpperCase()
+        setText(newText)
+    }
+
+    const handleLowClick = () => {
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState('Enter text here')
+
+    return (
+        <>
+            <div className='container my-3'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="Text" rows="5"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Covert to upper case</button>
+                <button className="btn btn-primary mx-2" onClick={handleLowClick}>Covert to lower case</button>
+            </div>
+            <div className="container my-3">
+                <h1>Your text summary</h1>
+                <p>{text.split(' ').length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(' ').length} Minutes read</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
+            </div>
+        </>
+    )
+}
+
+TextForm.prototype = {
+    heading: propTypes.string
+}
+
+TextForm.defaultProps = {
+    heading: "Heading"
+}
